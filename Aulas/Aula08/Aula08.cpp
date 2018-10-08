03dba85a2fd835713e4bcc1085621adf1f0c0989
NayraB <38119931+NayraB@users.noreply.github.com>
2018-10-08 09:37:00 -0300

    Variavel

--- Aulas/Aula08/Aula08.cpp
+++ Aulas/Aula08/Aula08.cpp
@@ -1,11 +1,32 @@
 // Aula08.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+
+/*
+Comentario longo, bloco
+(mais de uma linha)
+*/
+
+
+#include "stdafx.h" //inclusão de biblioteca 
+// C ANSI
+#include <stdio.h>
+#include <stdlib.h>
+
+// Variaveis (Globais, visiveis em todo o codigo)
+int a; // inteiro
+float b; //real (ponto flutuante)
+double c; // real (maior precisao)
+bool d; // booleano (verdadeiro/falso) (true/false)
 
 
 int main()
 {
+	// variavel local
+	//(visivel somente nesta funcao, a partir dessa linha)
+	int e;
+
+
     return 0;
 }
 
